206570b0e63b8674dfecbfc50bf7f92c6c9ae81a
Løwe <mathildeloewe@gmail.com>
2025-02-04 18:23:35 +0100

    add: dynamic prices and soldout

--- single.js
+++ single.js
@@ -8,9 +8,10 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${ProductId}`)
   .then((Response) => Response.json())
   .then((data) => {
     productContainer.innerHTML = `
-    <img src="https://kea-alt-del.dk/t7/images/webp/640/${ProductId}.webp" alt="" />
+    <img class="${data.soldout && "soldOut"}" src="https://kea-alt-del.dk/t7/images/webp/640/${ProductId}.webp" alt="" />
 
       <div class="product_info">
+      <h2 class="out ${data.soldout && "show"}">SOLD OUT</h2>
         <h1>${data.productdisplayname}</h1>
         <h4>${data.articletype} - ${data.usagetype}</h4>
 
@@ -21,8 +22,10 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${ProductId}`)
           <button class="large">L</button>
         </div>
 
-        <h3>DKK ${data.price},-</h3>
-        <button>Add to basket</button>
+       <h3 class="sale ${data.discount && "show"}">-${data.discount}%</h3>
+        <h3 class="sale ${data.discount && "show"}">Now DKK ${data.discount && Math.floor(data.price * (data.discount / 100))},-</h3>
+         <h3 class="${data.discount && "prev"}">DKK ${data.price},-</h3>
+        <button class="${data.soldout && "prev"}">Add to basket</button>
       </div>
         `;
   });
